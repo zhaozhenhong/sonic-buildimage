47f5736043b1b89198f41fe3e9eb1aaf219c0ac4
Zhenhong Zhao <Zhenhong_Zhao@dell.com>
2020-08-10 15:28:33 -0700

    Initial commit for adding non-template bgpcfgd script

--- dockers/docker-fpm-frr/docker_init.sh
+++ dockers/docker-fpm-frr/docker_init.sh
@@ -6,6 +6,7 @@ mkdir -p /etc/supervisor/conf.d
 sonic-cfggen -d -t /usr/share/sonic/templates/supervisord/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 
 CONFIG_TYPE=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["docker_routing_config_mode"]'`
+TEMPL_CONFIG=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["bgp_template_config"]'`
 
 if [[ ! -z "$NAMESPACE_ID" ]]; then
    # FRR is not running in host namespace so we need to delete
@@ -34,12 +35,17 @@ if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "separated" ]; then
     sonic-cfggen -d -t /usr/share/sonic/templates/bgpd/bgpd.conf.j2 -y /etc/sonic/constants.yml > /etc/frr/bgpd.conf
     sonic-cfggen -d -t /usr/share/sonic/templates/zebra/zebra.conf.j2 > /etc/frr/zebra.conf
     sonic-cfggen -d -t /usr/share/sonic/templates/staticd/staticd.conf.j2 > /etc/frr/staticd.conf
+    if [ -n "$TEMPL_CONFIG" ] && [ "$TEMPL_CONFIG" == "false" ]; then
+        sonic-cfggen -d -t /usr/share/sonic/templates/bgdd/bfdd.conf.j2 > /etc/frr/bfdd.conf
+        sonic-cfggen -d -t /usr/share/sonic/templates/ospfd/ospfd.conf.j2 > /etc/frr/ospfd.conf
+    fi
     echo "no service integrated-vtysh-config" > /etc/frr/vtysh.conf
     rm -f /etc/frr/frr.conf
 elif [ "$CONFIG_TYPE" == "unified" ]; then
     sonic-cfggen -d -y /etc/sonic/constants.yml -t /usr/share/sonic/templates/frr.conf.j2 >/etc/frr/frr.conf
     echo "service integrated-vtysh-config" > /etc/frr/vtysh.conf
     rm -f /etc/frr/bgpd.conf /etc/frr/zebra.conf /etc/frr/staticd.conf
+    rm -f /etc/frr/bfdd.conf /etc/frr/ospfd.conf /etc/frr/pimd.conf
 fi
 
 chown -R frr:frr /etc/frr/
